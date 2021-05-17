4cc0907f5a980550d8a129aac2a311243fac1ccb
Peter Koza <peter.koza@ibacz.eu>
2021-05-17 19:11:22 +0200

    demo

--- nfc-reader/src/App.js
+++ nfc-reader/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button style={{width:"100%", height:"100px", "font-size":"25px"}} onClick={readNFC}>SCAN</button>
   );
+}
+
+const readNFC = async () =>
+{
+  if ('NDEFReader' in window) {
+    try {
+      // eslint-disable-next-line no-undef
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      alert("Scan started successfully.");
+      ndef.onreadingerror = (event) => {
+        console.log(event);
+        alert(`Unsupported NFC tag.`);
+      };
+      ndef.onreading = event => {
+        console.log(event)
+        alert(`NDEF message read. Serial number: ${event.serialNumber}`);
+      };
+    } catch (error) {
+      alert(`Error! Scan failed to start: ${error}.`);
+    }
+  } else {
+    alert("No reader found. Ensure you have NFC activated. Visit https://developer.mozilla.org/en-US/docs/Web/API/Web_NFC_API#browser_compatibility for compatibility info");
+  }
 }
 
 export default App;
